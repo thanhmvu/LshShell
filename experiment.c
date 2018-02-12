032e038c256df44fd246539af2e8054c1369da70
HuyLafa <anhhuy0706@gmail.com>
2018-02-11 23:20:28 -0500

    Create project directory

---
+++ experiment.c
@@ -0,0 +1,16 @@
+#include "csapp.h"
+
+int main(int argc, char *argv[], char *envp[]) {
+	int i;
+	printf("Command line argument:\n");
+	for (i = 0 ; argv[i] != NULL; i++) {
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
+
+	printf("\n");
+	printf("Environment variables:\n");
+	for (i = 0 ; envp[i] != NULL ; i++) {
+		printf("envp[%d]: %s\n", i, envp[i]);
+	}
+	exit(0);
+}
